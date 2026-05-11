746cfa49adb7bbac76b8fafab9055b6c03a8181e
Alexander Bruck <alexander.bruck.santos@gmail.com>
2026-05-10 23:55:14 -0600

    página de perfil de librería

--- src/app/libreria/[id]/page.js
+++ src/app/libreria/[id]/page.js
@@ -129,12 +129,12 @@ export default function PerfilLibreria() {
         * { box-sizing: border-box; margin: 0; padding: 0; }
         body { font-family: 'Jost', sans-serif; }
         @keyframes pluma {
-          0%   { transform: translateX(0px)   rotate(0deg);  }
-          20%  { transform: translateX(-2px)  rotate(-7deg); }
-          50%  { transform: translateX(-5px)  rotate(4deg);  }
-          75%  { transform: translateX(-9px)  rotate(0deg);  }
-          80%  { transform: translateX(-9px)  rotate(-5deg); }
-          100% { transform: translateX(-9px)  rotate(0deg);  }
+          0%   { transform: translateX(-6px)  rotate(0deg);  }
+          20%  { transform: translateX(-4px)  rotate(7deg);  }
+          50%  { transform: translateX(-2px)  rotate(-4deg); }
+          75%  { transform: translateX(0px)   rotate(0deg);  }
+          80%  { transform: translateX(0px)   rotate(5deg);  }
+          100% { transform: translateX(0px)   rotate(0deg);  }
         }
         @keyframes trazar {
           0%   { stroke-dashoffset: 18; }
